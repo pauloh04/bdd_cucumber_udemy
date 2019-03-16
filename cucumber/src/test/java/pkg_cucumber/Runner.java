16e5521e917867b00a56b149c2e495f146d3912c
pauloh04 <pauloh04@gmail.com>
2019-03-16 00:15:38 -0300

    Init commit

---
+++ cucumber/src/test/java/pkg_cucumber/Runner.java
@@ -0,0 +1,13 @@
+package pkg_cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", monochrome = true, snippets = SnippetType.CAMELCASE)
+public class Runner {
+
+}
